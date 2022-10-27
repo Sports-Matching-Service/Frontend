bbe3ceeb26c0781eac856b745384ee551e19b9f7
Park-min-hyoung <qkralswgud1324@naver.com>
2022-10-27 17:06:08 +0900

    🐎 Add : ReviewPage
    Paging 기법 적용
    Paging 렌더링을 위해 임시 객체 배열 사용 (총 10개 페이지)
    Redux 미숙지로 Paging Component 분리 하지 않음

--- src/components/view/Review.jsx
+++ src/components/view/Review.jsx
@@ -1,8 +1,17 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import { Col, Row } from "react-bootstrap";
 import styled from "styled-components";
 import { FaUserAlt } from "react-icons/fa";
 import { BsFillStarFill } from "react-icons/bs";
+import { calcTime } from "../../js/calcTime";
+import { divdePageGroup } from "../../js/divdePageGroup";
+import {
+  BiArrowFromRight,
+  BiArrowFromLeft,
+  BiRightArrowAlt,
+  BiLeftArrowAlt,
+} from "react-icons/bi";
 import { Link } from "react-router-dom";
 import { useQuery } from "react-query";
 import axios from "axios";
@@ -63,7 +72,6 @@ const ReviewContainers = styled.div`
   width: 100%;
   display: flex;
   flex-direction: column;
-  margin-bottom: 70px; ;
 `;
 
 const ReviewContainer = styled.div`
@@ -87,6 +95,367 @@ const ReviewUser = styled.div`
 
 const ReviewContent = styled.div``;
 
+const PagingContainer = styled.div`
+  width: 40%;
+  margin: 70px auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  & svg {
+    cursor: pointer;
+    width: 30px;
+    height: 30px;
+  }
+`;
+
+const Button = styled.button`
+  background-color: ${(props) => props["aria-current"] === "page" && "yellow"};
+  width: 50px;
+  height: 50px;
+  margin: 0 10px;
+  border: none;
+  border-radius: 50%;
+  transition: all 0.5s ease;
+
+  &:hover {
+    background-color: orange;
+  }
+`;
+
+const LIMIT = 5;
+const writerData = [
+  [
+    {
+      name: "박민형",
+      position: "울산 광역시 동구",
+      time: "2022-10-25T03:24:00",
+      content: "친절하십니다.",
+    },
+    {
+      name: "박민형",
+      position: "울산 광역시 동구",
+      time: "2022-10-25T14:24:00",
+      content: "친절하십니다.",
+    },
+    {
+      name: "박민형",
+      position: "울산 광역시 동구",
+      time: "2022-10-24T03:24:00",
+      content: "친절하십니다.",
+    },
+    {
+      name: "박민형",
+      position: "울산 광역시 동구",
+      time: "2022-10-24T03:24:00",
+      content: "친절하십니다.",
+    },
+    {
+      name: "박민형",
+      position: "울산 광역시 동구",
+      time: "2022-10-24T03:24:00",
+      content: "친절하십니다.",
+    },
+  ],
+
+  [
+    {
+      name: "메시",
+      position: "울산 광역시 동구",
+      time: "2022-10-25T03:24:00",
+      content: "친절하십니다.",
+    },
+    {
+      name: "메시",
+      position: "울산 광역시 동구",
+      time: "2022-10-25T14:24:00",
+      content: "친절하십니다.",
+    },
+    {
+      name: "메시",
+      position: "울산 광역시 동구",
+      time: "2022-10-24T03:24:00",
+      content: "친절하십니다.",
+    },
+    {
+      name: "메시",
+      position: "울산 광역시 동구",
+      time: "2022-10-24T03:24:00",
+      content: "친절하십니다.",
+    },
+    {
+      name: "메시",
+      position: "울산 광역시 동구",
+      time: "2022-10-24T03:24:00",
+      content: "친절하십니다.",
+    },
+  ],
+
+  [
+    {
+      name: "손흥민",
+      position: "울산 광역시 동구",
+      time: "2022-10-25T03:24:00",
+      content: "친절하십니다.",
+    },
+    {
+      name: "손흥민",
+      position: "울산 광역시 동구",
+      time: "2022-10-25T14:24:00",
+      content: "친절하십니다.",
+    },
+    {
+      name: "손흥민",
+      position: "울산 광역시 동구",
+      time: "2022-10-24T03:24:00",
+      content: "친절하십니다.",
+    },
+    {
+      name: "손흥민",
+      position: "울산 광역시 동구",
+      time: "2022-10-24T03:24:00",
+      content: "친절하십니다.",
+    },
+    {
+      name: "손흥민",
+      position: "울산 광역시 동구",
+      time: "2022-10-24T03:24:00",
+      content: "친절하십니다.",
+    },
+  ],
+
+  [
+    {
+      name: "조르지뉴",
+      position: "울산 광역시 동구",
+      time: "2022-10-25T03:24:00",
+      content: "친절하십니다.",
+    },
+    {
+      name: "조르지뉴",
+      position: "울산 광역시 동구",
+      time: "2022-10-25T14:24:00",
+      content: "친절하십니다.",
+    },
+    {
+      name: "조르지뉴",
+      position: "울산 광역시 동구",
+      time: "2022-10-24T03:24:00",
+      content: "친절하십니다.",
+    },
+    {
+      name: "조르지뉴",
+      position: "울산 광역시 동구",
+      time: "2022-10-24T03:24:00",
+      content: "친절하십니다.",
+    },
+    {
+      name: "조르지뉴",
+      position: "울산 광역시 동구",
+      time: "2022-10-24T03:24:00",
+      content: "친절하십니다.",
+    },
+  ],
+
+  [
+    {
+      name: "김민재",
+      position: "울산 광역시 동구",
+      time: "2022-10-25T03:24:00",
+      content: "친절하십니다.",
+    },
+    {
+      name: "김민재",
+      position: "울산 광역시 동구",
+      time: "2022-10-25T14:24:00",
+      content: "친절하십니다.",
+    },
+    {
+      name: "김민재",
+      position: "울산 광역시 동구",
+      time: "2022-10-24T03:24:00",
+      content: "친절하십니다.",
+    },
+    {
+      name: "김민재",
+      position: "울산 광역시 동구",
+      time: "2022-10-24T03:24:00",
+      content: "친절하십니다.",
+    },
+    {
+      name: "김민재",
+      position: "울산 광역시 동구",
+      time: "2022-10-24T03:24:00",
+      content: "친절하십니다.",
+    },
+  ],
+
+  [
+    {
+      name: "박민형",
+      position: "울산 광역시 동구",
+      time: "2022-10-25T03:24:00",
+      content: "친절하십니다.",
+    },
+    {
+      name: "박민형",
+      position: "울산 광역시 동구",
+      time: "2022-10-25T14:24:00",
+      content: "친절하십니다.",
+    },
+    {
+      name: "박민형",
+      position: "울산 광역시 동구",
+      time: "2022-10-24T03:24:00",
+      content: "친절하십니다.",
+    },
+    {
+      name: "박민형",
+      position: "울산 광역시 동구",
+      time: "2022-10-24T03:24:00",
+      content: "친절하십니다.",
+    },
+    {
+      name: "박민형",
+      position: "울산 광역시 동구",
+      time: "2022-10-24T03:24:00",
+      content: "친절하십니다.",
+    },
+  ],
+
+  [
+    {
+      name: "메시",
+      position: "울산 광역시 동구",
+      time: "2022-10-25T03:24:00",
+      content: "친절하십니다.",
+    },
+    {
+      name: "메시",
+      position: "울산 광역시 동구",
+      time: "2022-10-25T14:24:00",
+      content: "친절하십니다.",
+    },
+    {
+      name: "메시",
+      position: "울산 광역시 동구",
+      time: "2022-10-24T03:24:00",
+      content: "친절하십니다.",
+    },
+    {
+      name: "메시",
+      position: "울산 광역시 동구",
+      time: "2022-10-24T03:24:00",
+      content: "친절하십니다.",
+    },
+    {
+      name: "메시",
+      position: "울산 광역시 동구",
+      time: "2022-10-24T03:24:00",
+      content: "친절하십니다.",
+    },
+  ],
+
+  [
+    {
+      name: "손흥민",
+      position: "울산 광역시 동구",
+      time: "2022-10-25T03:24:00",
+      content: "친절하십니다.",
+    },
+    {
+      name: "손흥민",
+      position: "울산 광역시 동구",
+      time: "2022-10-25T14:24:00",
+      content: "친절하십니다.",
+    },
+    {
+      name: "손흥민",
+      position: "울산 광역시 동구",
+      time: "2022-10-24T03:24:00",
+      content: "친절하십니다.",
+    },
+    {
+      name: "손흥민",
+      position: "울산 광역시 동구",
+      time: "2022-10-24T03:24:00",
+      content: "친절하십니다.",
+    },
+    {
+      name: "손흥민",
+      position: "울산 광역시 동구",
+      time: "2022-10-24T03:24:00",
+      content: "친절하십니다.",
+    },
+  ],
+
+  [
+    {
+      name: "조르지뉴",
+      position: "울산 광역시 동구",
+      time: "2022-10-25T03:24:00",
+      content: "친절하십니다.",
+    },
+    {
+      name: "조르지뉴",
+      position: "울산 광역시 동구",
+      time: "2022-10-25T14:24:00",
+      content: "친절하십니다.",
+    },
+    {
+      name: "조르지뉴",
+      position: "울산 광역시 동구",
+      time: "2022-10-24T03:24:00",
+      content: "친절하십니다.",
+    },
+    {
+      name: "조르지뉴",
+      position: "울산 광역시 동구",
+      time: "2022-10-24T03:24:00",
+      content: "친절하십니다.",
+    },
+    {
+      name: "조르지뉴",
+      position: "울산 광역시 동구",
+      time: "2022-10-24T03:24:00",
+      content: "친절하십니다.",
+    },
+  ],
+
+  [
+    {
+      name: "김민재",
+      position: "울산 광역시 동구",
+      time: "2022-10-25T03:24:00",
+      content: "친절하십니다.",
+    },
+    {
+      name: "김민재",
+      position: "울산 광역시 동구",
+      time: "2022-10-25T14:24:00",
+      content: "친절하십니다.",
+    },
+    {
+      name: "김민재",
+      position: "울산 광역시 동구",
+      time: "2022-10-24T03:24:00",
+      content: "친절하십니다.",
+    },
+    {
+      name: "김민재",
+      position: "울산 광역시 동구",
+      time: "2022-10-24T03:24:00",
+      content: "친절하십니다.",
+    },
+    {
+      name: "김민재",
+      position: "울산 광역시 동구",
+      time: "2022-10-24T03:24:00",
+      content: "친절하십니다.",
+    },
+  ],
+];
+
 const userData = {
   name: "박민형",
   email: "qkralswgud1324@naver.com",
@@ -98,102 +467,7 @@ const reviewData = {
   count: 2000,
 };
 
-const writerData = [
-  {
-    name: "박민형",
-    position: "울산 광역시 동구",
-    time: "2022-10-25T03:24:00",
-    content: "친절하십니다.",
-  },
-  {
-    name: "박민형",
-    position: "울산 광역시 동구",
-    time: "2022-10-25T14:24:00",
-    content: "친절하십니다.",
-  },
-  {
-    name: "박민형",
-    position: "울산 광역시 동구",
-    time: "2022-10-24T03:24:00",
-    content: "친절하십니다.",
-  },
-  {
-    name: "박민형",
-    position: "울산 광역시 동구",
-    time: "2022-10-24T03:24:00",
-    content: "친절하십니다.",
-  },
-  {
-    name: "박민형",
-    position: "울산 광역시 동구",
-    time: "2022-10-24T03:24:00",
-    content: "친절하십니다.",
-  },
-  {
-    name: "박민형",
-    position: "울산 광역시 동구",
-    time: "2022-10-24T03:24:00",
-    content: "친절하십니다.",
-  },
-  {
-    name: "박민형",
-    position: "울산 광역시 동구",
-    time: "2022-10-24T03:24:00",
-    content: "친절하십니다.",
-  },
-  {
-    name: "박민형",
-    position: "울산 광역시 동구",
-    time: "2022-10-24T03:24:00",
-    content: "친절하십니다.",
-  },
-  {
-    name: "박민형",
-    position: "울산 광역시 동구",
-    time: "2022-10-24T03:24:00",
-    content: "친절하십니다.",
-  },
-  {
-    name: "박민형",
-    position: "울산 광역시 동구",
-    time: "2022-10-24T03:24:00",
-    content: "친절하십니다.",
-  },
-  {
-    name: "박민형",
-    position: "울산 광역시 동구",
-    time: "2022-10-24T03:24:00",
-    content: "친절하십니다.",
-  },
-];
-
 function Review() {
-  // by 민형, 시간 변환 로직_221025
-  const calcTime = (value) => {
-    const today = new Date();
-    const timeValue = new Date(value);
-
-    const betweenTime = Math.floor(
-      (today.getTime() - timeValue.getTime()) / 1000 / 60
-    );
-    if (betweenTime < 1) return "방금전";
-    if (betweenTime < 60) {
-      return `${betweenTime}분전`;
-    }
-
-    const betweenTimeHour = Math.floor(betweenTime / 60);
-    if (betweenTimeHour < 24) {
-      return `${betweenTimeHour}시간전`;
-    }
-
-    const betweenTimeDay = Math.floor(betweenTime / 60 / 24);
-    if (betweenTimeDay < 365) {
-      return `${betweenTimeDay}일전`;
-    }
-
-    return `${Math.floor(betweenTimeDay / 365)}년전`;
-  };
-
   // by 민형, User(이름, 이메일, 주소) | Review(별점, 리뷰 개수) | 작성자(이름, 주소, 댓글내용, 시간) 데이터 가져오기_221025
 
   //   const { isLoading: userLoading, data: userData } = useQuery("user", () =>
@@ -208,8 +482,9 @@ function Review() {
   //     () => axios.get(`API URL/writer`)
   //   );
 
+  const [page, setPage] = useState(1);
   // by 민형, DB에서 받은 시간 데이터를 사용자가 알아 볼 수 있도록 변환_221025
-  const timeWirteData = writerData.map((item) => {
+  const timeWirteData = writerData[page - 1].map((item) => {
     return {
       name: item.name,
       position: item.position,
@@ -218,7 +493,65 @@ function Review() {
     };
   });
 
-  //   const loading = userLoading || reviewLoading || writerLoading || !timeWirteData;
+  // by 민형, 여기 밑에 부터 Paging 소스코드_221027
+
+  // by 민형, 나중에 백엔드에서 데이터 받아와서 사용할 코드들_221027
+  // Math.ceil(groundList.length / listPerPage);
+  // const listPerPage = 8;
+
+  const [matchList, setMatchList] = useState([]);
+  const totalPage = writerData.length;
+
+  // by 민형, 특정 페이지 그룹 배열을 가지고 있음([1,2,3,4,5], [6,7,8,9,10])_221026
+  const [currentPageGroup, setCurrentPageGroup] = useState([]);
+  // by 민형, 모든 페이지 그룹을 가지고 있음_221026
+  const [totalPageGroup, setTotalPageGroup] = useState([]);
+
+  // by 민형, 페이지 단위로 back에서 데이터를 받아오는 소스코드_221027
+  useEffect(() => {
+    (async () => {
+      // const result = await Api.get(
+      //   `grounds?location=${location}&search=${searchInput}&offset=${
+      //     (page - 1) * listPerPage
+      //   }&count=${listPerPage}`
+      // );
+      // console.log(DATA[page - 1]);
+      // console.log(...DATA[page - 1]);
+      setMatchList(writerData[page - 1]);
+
+      // setMatchList({
+      //   length: result.data.length,
+      //   data: result.data.grounds,
+      // });
+    })();
+  }, [page]);
+
+  useEffect(() => {
+    // by 민형, 페이지 그룹의 시작 page 라면_221027
+    if (page % LIMIT === 1) {
+      // by 민형, 여러개의 페이지 그룹에서 조건에 부합한 페이지 그룹 선택해서 state 변경_2210626
+      setCurrentPageGroup(totalPageGroup[Math.floor(page / LIMIT)]);
+    }
+    // by 민형, 페이지 그룹의 마지막 page 라면_221027
+    else if (page % LIMIT === 0) {
+      setCurrentPageGroup(totalPageGroup[Math.floor(page / LIMIT) - 1]);
+    } else if (page === totalPage) {
+      setCurrentPageGroup(totalPageGroup[Math.floor(page / LIMIT)]);
+    }
+  }, [page]);
+
+  useEffect(() => {
+    const slicedPageGroup = divdePageGroup(totalPage, LIMIT);
+    setTotalPageGroup(slicedPageGroup);
+    // by 민형, 각 페이지그룹의 마지막페이지는 그냥 LIMIT으로 나눠주면 다음 페이지 그룹이 렌더링 된다_221026
+    // 5 / 5 = 1 이므로 2번 페이지 그룹 렌더링(0이 1번 그룹이므로)
+    // 10 / 5 = 2 이므로 3번 페이지 그룹 렌더링 되므로 각 그룹의 마지막 페이지는 -1을 한후 나누기
+    const targetGroupNumber =
+      page % LIMIT !== 0
+        ? Math.floor(page / LIMIT)
+        : Math.floor((page - 1) / LIMIT);
+    setCurrentPageGroup(slicedPageGroup[targetGroupNumber]);
+  }, [totalPage]);
 
   return (
     <>
@@ -262,6 +595,31 @@ function Review() {
           </ReviewContainers>
         </Col>
       </Row>
+      <Col sm={12} md={12}>
+        <PagingContainer>
+          <BiArrowFromRight onClick={() => setPage(1)} />
+          <BiLeftArrowAlt
+            onClick={page !== 1 ? () => setPage(page - 1) : null}
+          />
+
+          <div>
+            {currentPageGroup?.map((i) => (
+              <Button
+                key={i + 1}
+                onClick={() => setPage(i + 1)}
+                aria-current={page === i + 1 ? "page" : null}
+              >
+                {i + 1}
+              </Button>
+            ))}
+          </div>
+
+          <BiRightArrowAlt
+            onClick={page !== totalPage ? () => setPage(page + 1) : null}
+          />
+          <BiArrowFromLeft onClick={() => setPage(totalPage)} />
+        </PagingContainer>
+      </Col>
     </>
   );
 }
